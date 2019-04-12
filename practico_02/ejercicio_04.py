f69de5101c735c670d437e6b2d85d504ea4a1c3e
gonzalormonja <48839385+gonzalormonja@users.noreply.github.com>
2019-04-12 15:15:03 -0300

    Tp2 (#3)
    * Update ejercicio_01.py
    
    * Update ejercicio_02.py
    
    * Update ejercicio_03.py
    
    * Update ejercicio_03.py
    
    * Update ejercicio_04.py
    
    * Update ejercicio_05.py
    
    * Update ejercicio_06.py
    
    * Correcion ejercicio 4
    
    * Correcion ejercicio 05

--- practico_02/ejercicio_04.py
+++ practico_02/ejercicio_04.py
@@ -1,3 +1,4 @@
+
 # Escribir una clase Estudiante, que herede de Persona, y que agregue las siguientes condiciones:
 # Atributos:
 # - nombre de la carrera.
@@ -8,15 +9,34 @@
 # - avance(): indica que porcentaje de la carrera tiene aprobada.
 # - edad_ingreso(): indica que edad tenia al ingresar a la carrera (basándose en el año actual).
 
+import random
+from datetime import datetime
+from practico_02.ejercicio_03 import Persona
 
-class Estudiante:
+class Estudiante(Persona):
 
-    def __init__(self, carrera, anio, cantidad_materias, cantidad_aprobadas):
+    def __init__(self, nombre, edad, sexo, peso, altura, carrera, anio, cantidad_materias, cantidad_aprobadas):
+        super().__init__(nombre, edad, sexo, peso, altura)
+        self.carrera = carrera
+        self.anio = anio
+        self.cantidad_materias = cantidad_materias
+        self.cantidad_aprobadas = cantidad_aprobadas
+
         pass
 
     def avance(self):
+        return (self.cantidad_aprobadas*100/self.cantidad_materias)
         pass
 
     # implementar usando modulo datetime
     def edad_ingreso(self):
+        fecha = datetime.now().date().year
+        aniosEstudiando = fecha-self.anio
+        return self.edad-aniosEstudiando
         pass
+
+
+est = Estudiante("Juan",22,"H",75,1.78,"Ingenieria en Sistemas",2016,50,25)
+assert est.avance() == 50
+assert est.edad_ingreso() == 19
+
--- practico_02/ejercicio_05.py
+++ practico_02/ejercicio_05.py
@@ -2,7 +2,32 @@
 # y devuelva un diccionario con las carreras como keys, y la cantidad de estudiantes en cada una de ellas como values.
 
 from practico_02.ejercicio_04 import Estudiante
-
-
 def organizar_estudiantes(estudiantes):
-    pass
+    dic = {}
+    for e in estudiantes:
+        if e.carrera in dic:
+            dic[e.carrera] = int(dic[e.carrera])+1
+        else:
+            dic[e.carrera] = 1
+
+    return dic
+
+est = []
+e = Estudiante("Juan",22,"H",75,1.78,"isi",2015,10,5)
+est.append(e)
+e = Estudiante("Pedro",19,"H",72,1.79,"iq",2015,10,5)
+est.append(e)
+e = Estudiante("Gonzalo",25,"H",80,1.90,"isi",2015,10,5)
+est.append(e)
+e = Estudiante("Federica",21,"M",60,1.78,"im",2015,10,5)
+est.append(e)
+e = Estudiante("Julia",22,"M",77,1.78,"isi",2015,10,5)
+est.append(e)
+e = Estudiante("Martin",22,"H",75,1.65,"iq",2015,10,5)
+est.append(e)
+e = Estudiante("Nicolas",23,"H",96,2.00,"ie",2015,10,5)
+est.append(e)
+e = Estudiante("Manuel",22,"H",75,1.78,"ii",2015,10,5)
+est.append(e)
+
+assert organizar_estudiantes(est) == {'isi': 3, 'iq': 2, 'im': 1, 'ie': 1, 'ii': 1}
