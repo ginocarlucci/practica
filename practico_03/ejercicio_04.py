3cf5f1429723a5c34ddfdcf510341971732bffd2
gonzalormonja <48839385+gonzalormonja@users.noreply.github.com>
2019-04-12 16:37:16 -0300

    Update ejercicio_04.py

--- practico_03/ejercicio_04.py
+++ practico_03/ejercicio_04.py
@@ -3,19 +3,27 @@
 # Si no encuentra ningun registro, devuelve False.
 
 import datetime
+import sqlite3
 
 from practico_03.ejercicio_01 import reset_tabla
 from practico_03.ejercicio_02 import agregar_persona
 
-
 def buscar_persona(id_persona):
-    return False
-
+    db = sqlite3.connect('mibase')
+    cursor = db.cursor()
+    cSQL = 'SELECT * FROM Personas WHERE idPersona = '+str(id_persona)
+    cursor.execute(cSQL)
+    db.commit()
+    elem = cursor.fetchone()
+    if(elem):
+        return elem
+    else:
+        return False
 
 @reset_tabla
 def pruebas():
     juan = buscar_persona(agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180))
-    assert juan == (1, 'juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
+    assert juan == (1, 'juan perez',  '1988-05-15 00:00:00', 32165498, 180)
     assert buscar_persona(12345) is False
 
 if __name__ == '__main__':
