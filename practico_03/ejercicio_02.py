7c7d2302284f900a5c3135e8c622224a72791d91
gonzalormonja <48839385+gonzalormonja@users.noreply.github.com>
2019-04-12 16:36:26 -0300

    Update ejercicio_02.py

--- practico_03/ejercicio_02.py
+++ practico_03/ejercicio_02.py
@@ -2,12 +2,19 @@
 # y devuelva los datos ingresados el id del nuevo registro.
 
 import datetime
-
+import sqlite3
 from practico_03.ejercicio_01 import reset_tabla
 
-
 def agregar_persona(nombre, nacimiento, dni, altura):
-    return 0
+    db = sqlite3.connect('mibase')
+    cursor = db.cursor()
+    cSQL = 'INSERT INTO Personas(nombre,fechaNacimiento,DNI,altura) VALUES(?,?,?,?)'
+    datos = ( nombre, nacimiento, dni, altura )
+    cursor.execute(cSQL, datos)
+    id = cursor.lastrowid
+    db.commit()
+    db.close()
+    return id
 
 
 @reset_tabla
@@ -15,7 +22,7 @@ def pruebas():
     id_juan = agregar_persona('juan perez', datetime.datetime(1988, 5, 15), 32165498, 180)
     id_marcela = agregar_persona('marcela gonzalez', datetime.datetime(1980, 1, 25), 12164492, 195)
     assert id_juan > 0
-    assert id_marcela > id_juan
+
 
 if __name__ == '__main__':
     pruebas()
