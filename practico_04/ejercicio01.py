9eaa4996a1b1dfcc1956b53853b71868a7a2ff2a
= <=>
2019-05-24 16:33:41 -0300

    inicio

--- practico_04/ejercicio01.py
+++ practico_04/ejercicio01.py
@@ -1,5 +1,43 @@
-## 1 Ejercicio Hacer un formulario tkinter que es una calculadora, tiene 2 entry para ingresar los valores V1 y V2.
-## Y 4 botones de operaciones para las operaciones respectivas + , - , * , / ,
-## al cliquearlos muestre el resultado de aplicar el operador respectivo en los V1 y V2 . 
- 
+# 1 Ejercicio Hacer un formulario tkinter que es una calculadora, tiene 2 entry para ingresar los valores V1 y V2.
+# Y 4 botones de operaciones para las operaciones respectivas + , - ,import  * , / ,
+# al cliquearlos import muestre el resultado de aplicar el operador respectivo en los V1 y V2 .
+
+from tkinter import *
+
+
+def sumar():
+    total.set(v1.get()+v2.get())
+
+
+def restar():
+    total.set(v1.get()-v2.get())
+
+
+def multiplicar():
+    total.set(v1.get()*v2.get())
+
+
+def dividir():
+    total.set(v1.get()/v2.get())
+
+
+root = Tk()
+root.title("Calculadora")
+Label(root,text="Primer numero").pack()
+v1 = IntVar()
+Entry(root,textvariable=v1).pack()
+Label(root,text="segundo numero").pack()
+v2 = IntVar()
+Entry(root,textvariable=v2).pack()
+
+Button(root,text = "+",command=sumar).pack(side=LEFT)
+Button(root,text = "-",command=restar).pack(side=LEFT)
+total = IntVar()
+Label(root,text="",textvariable=total).pack(side=LEFT)
+Button(root,text = "*",command=multiplicar).pack(side=LEFT)
+Button(root,text = "/",command=dividir).pack(side=LEFT)
+
+root.mainloop()
+
+
 
--- practico_04/ejercicio02.py
+++ practico_04/ejercicio02.py
@@ -1,3 +1,37 @@
 ## 2 Ejercicio Hacer un formulario en Tkinter una calculadora que tenga 1 entry y 12 botones para los dígitos 0 al 9
 ## y las operaciones + - / * = , que al apretar cada botón vaya agregando al valor que muestra en el entry el carácter 
 ## que le corresponde ( como se ve imagen ) y cuando se aprieta en = pone el resultado de evaluar la cadena entrada . 
+
+from tkinter import *
+def sumar():
+    pass
+def restar():
+    pass
+def multiplicar():
+    pass
+def dividir():
+    pass
+def uno():
+    pass
+
+
+root = Tk()
+root.title("Calculadora")
+total = DoubleVar()
+vp = Frame(root)
+vp.grid(column=0, row=0, padx=(50,50), pady=(10,10))
+Entry(vp,text = "total",textvariable = total).grid(column = 0, row = 0)
+
+vp2 = Frame(root)
+vp2.grid(column=0, row=1, padx=(50,50), pady=(10,10))
+
+Button(vp2,text = "7",command = uno).grid(column = 0,row=1)
+Button(vp2,text = "8",command = uno).grid(column = 1,row=1)
+Button(vp2,text = "9",command = uno).grid(column = 2,row=1)
+Button(vp2,text = "4",command = uno).grid(column = 0, row=2)
+Button(vp2,text = "5",command = uno).grid(column = 1, row=2)
+Button(vp2,text = "6",command = uno).grid(column = 2, row=2)
+Button(vp2,text = "1",command = uno).grid(column = 0, row=3)
+Button(vp2,text = "2",command = uno).grid(column = 1, row=3)
+Button(vp2,text = "3",command = uno).grid(column = 2, row=3)
+root.mainloop()
