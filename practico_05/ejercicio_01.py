08d5cff84055cde705413f127d2f7a6702d3b4c6
= <=>
2019-06-12 12:38:03 -0300

    toque sin querer

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -19,7 +19,6 @@ class Socio(Base):
     nombre = Column(String(250))
     apellido = Column(String(250))
 
-#engine = create_engine('sqlite://')
 engine = create_engine('sqlite://sqlalchemy_ejemplo0.db')
 
 Base.metadata.bind = engine
