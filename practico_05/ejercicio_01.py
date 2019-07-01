3222ab0fb8f1a9832aba3b71fa13203acc368d4d
= <=>
2019-07-01 15:41:53 -0300

    resuelvo merge

--- practico_05/ejercicio_01.py
+++ practico_05/ejercicio_01.py
@@ -11,3 +11,7 @@ Base = declarative_base()
 
 class Socio(Base):
     __tablename__ = 'socios'
+    id = Column(Integer, primary_key=True)
+    dni = Column(Integer, unique=True)
+    nombre = Column(String(250))
+    apellido = Column(String(250))
