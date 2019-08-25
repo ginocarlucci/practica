6e791ab86076e10be1e713b2367826328f7f613a
Nano <ginocarlucci@hotmail.com>
2019-08-25 16:52:33 -0300

    Mejoro formulario Tkinter

--- practico_07/practico_07.py
+++ practico_07/practico_07.py
@@ -1,15 +1,10 @@
 """
 Tp7 – Capa Presentacion Socios
-
 Crear en Python usando Tkinter un formulario para gestionar los datos de Socios usando la Clase de la Capa de Negocio Socios .
-
 El Formulario principal tiene que mostrar todos los socios en Treeview y tener los siguientes botones Alta , Baja , Modificar .
-
 Apretar el Boton Alta se tiene que abrir un formulario con los campos para ingresar los datos de socio .
 Incluye  2 botones Guardar y Cancelar.
-
 Apretar el Boton Baja se tiene que dar de baja el socio seleccionado .
-
 Apretar el Boton Modificar se tiene que abrir un formulario con los campos con los datos del socio seleccionado .
 Incluye 2 botones Aceptar y Cancelar .
 """
@@ -23,7 +18,7 @@ from practico_05.ejercicio_01 import Socio
 def alta():
     e = Toplevel(root)
     e.title("Ingresar nuevo Socio")
-    e.geometry('380x200')
+    e.geometry('250x120')
     Label(e, text="Ingrese el Dni").grid(column=0, row=0)
     dni = IntVar()
     Entry(e, textvariable=dni).grid(column=1, row=0)
@@ -43,7 +38,7 @@ def alta():
         socioNegocio = capa_negocio.DatosSocio()
         socioNegocio.alta(socio)
         socio = socioNegocio.buscar_dni(dni.get())
-        tree.insert("", tk.END, text=socio.id, values=(socio.dni, socio.apellido, socio.nombre))
+        tree.insert("", tk.END, text=socio.id, values=(socio.dni, socio.nombre, socio.apellido))
         e.destroy()
 
 def baja():
@@ -62,7 +57,7 @@ def modificacion():
 
     e = Toplevel(root)
     e.title("Ingresar nuevo Socio")
-    e.geometry('380x200')
+    e.geometry('250x120')
     Label(e, text="Ingrese el Dni").grid(column=0, row=0)
     dni = IntVar()
     dni.set(socio.dni)
@@ -86,33 +81,39 @@ def modificacion():
         socio.nombre = nombre.get()
         socio.apellido = apellido.get()
         socioNegocio.modificacion(socio)
-        tree.item(item,text=socio.id, values=(socio.dni,socio.apellido,socio.nombre))
+        tree.item(item,text=socio.id, values=(socio.dni,socio.nombre,socio.apellido))
         e.destroy()
 
 
-root = Tk();
-root.title("ABM Socios")
+def cargarTabla(tree):
+    tree["columns"] = ("dni", "nombre", "apellido")
+    tree.column('#0',width=100)
+    tree.column('dni',width=150)
+    tree.column('nombre',width=150)
+    tree.column('apellido',width=150)
+    tree.heading('#0', text='ID')
+    tree.heading('dni',text='DNI')
+    tree.heading('nombre',text='Nombre')
+    tree.heading('apellido',text='apellido')
 
-columnas = 4;
-negocioSocio = capa_negocio.NegocioSocio()
-totalSocios = negocioSocio.todos()
-filas = len(totalSocios)
+    negocioSocio = capa_negocio.NegocioSocio()
+    totalSocios = negocioSocio.todos()
 
-tree=ttk.Treeview()
-tree["columns"]=("nombre","apellido","DNI")
+    for i in totalSocios:
+        tree.insert("", tk.END, text=i.id, values=(i.dni, i.apellido, i.nombre))
+    tree.pack()
 
-tree.heading("#0", text="ID");
-tree.heading("nombre", text="DNI");
-tree.heading("apellido", text="Apellido");
-tree.heading("DNI", text="Nombre");
+root = Tk()
+root.resizable(False,False)
+root.geometry('600x200')
+root.configure(bg = 'beige')
+root.title('-- ABM SOCIOS --')
+ttk.Button(root, text='Salir',command=root.destroy).pack(side=BOTTOM)
+tree = ttk.Treeview()
+cargarTabla(tree)
 
-for i in totalSocios:
-    tree.insert("",tk.END, text=i.id, values=(i.dni,i.apellido,i.nombre))
-
-tree.pack()
-
-Button(root,text = "Alta",command=alta).pack(side=LEFT)
-Button(root,text = "Baja",command=baja).pack(side=LEFT)
-Button(root,text = "Modificacion",command=modificacion).pack(side=LEFT)
+ttk.Button(root,text='alta',command=alta).place(x=20,y=175)
+ttk.Button(root,text='modificar',command=modificacion).place(x=100,y=175)
+ttk.Button(root,text='eliminar',command=baja).place(x=180,y=175)
 
 root.mainloop()
